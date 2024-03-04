eb7398ec13a264828a46278e004712638483a314
radny <poradzisz.pawel@gmail.com>
2024-03-04 11:28:33 +0100

    Pylint

--- tests.py
+++ tests.py
@@ -2,16 +2,22 @@ import pytest
 from hashing import Hashing
 
 def test_hashing_short_password():
+    """_summary_
+    """
     password = "a82aff09"
     with pytest.raises(Exception):
         Hashing.create_hash(password=password)
 
 def test_hashing_short_salt():
+    """_summary_
+    """
     salt = "a82a888"
     with pytest.raises(Exception):
         Hashing.create_hash(salt=salt)
 
 def test_hashing_less_iterations():
+    """_summary_
+    """
     iterations = 99
     with pytest.raises(Exception):
         Hashing.create_hash(iterations=iterations)
@@ -24,8 +30,17 @@ def test_hashing_less_iterations():
     ]   
 )
 def test_hash_is_created(password, salt, iterations):
+    """_summary_
+
+    Args:
+        password (_type_): _description_
+        salt (_type_): _description_
+        iterations (_type_): _description_
+    """
     Hashing.create_hash(password=password, salt=salt, iterations=iterations)
     assert len(Hashing.create_hash(password=password, salt=salt, iterations=iterations))==4
 
 def test_hash_is_created2_without_parameters():
+    """_summary_
+    """
     assert len(Hashing.create_hash())==4
